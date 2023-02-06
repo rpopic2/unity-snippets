034152670f9d722340e09e2ba4013d68ab4c003c
rpopic <rpopic2@gmail.com>
2023-02-06 15:33:54 +0900

    Create SceneSwitcherEditorWindow.cs

---
+++ SceneSwitcherEditorWindow.cs
@@ -0,0 +1,45 @@
+// Author: rpopic2 (github.com/rpopic2)
+// Last Modified: 2023-02-06
+// Description: A simple scene switcher window for Unity Editor
+using UnityEditor;
+using UnityEngine;
+using UnityEditor.SceneManagement;
+using System.Collections.Generic;
+
+public class SceneSwitcherEditorWindow : EditorWindow
+{
+    private Dictionary<string, string> scenes = new Dictionary<string, string>();
+    [MenuItem("Tools/SceneSwitcher")]
+    private static void Init()
+    {
+        EditorWindow.GetWindow(typeof(SceneSwitcherEditorWindow), false, "씬 전환기", true);
+    }
+    private void OnEnable()
+    {
+        Refresh();
+    }
+    private void OnGUI()
+    {
+        foreach (var (name, path) in scenes)
+        {
+            if (GUILayout.Button(name, GUILayout.Height(30)))
+            {
+                EditorSceneManager.OpenScene(path);
+            }
+        }
+        if (GUILayout.Button("Refresh", GUILayout.Width(100)))
+        {
+            Refresh();
+        }
+    }
+    private void Refresh()
+    {
+        foreach (var s in EditorBuildSettings.scenes)
+        {
+            var path = s.path;
+            var slashLastIndex = path.LastIndexOf('/');
+            var sceneName = path.Substring(slashLastIndex + 1, path.LastIndexOf('.') - slashLastIndex - 1);
+            scenes.Add(sceneName, path);
+        }
+    }
+}
