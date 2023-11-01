b6eaf427a58b4e8b581aa57f50c410b7a7f028f7
rpopic2 <8055265+rpopic2@users.noreply.github.com>
2023-11-01 15:14:59 +0900

    update Singleton.cs

--- Singleton.cs
+++ Singleton.cs
@@ -4,7 +4,7 @@ public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
 {
     public static T Instance { get; private set; }
 
-    protected void Init(T self)
+    protected void SingletonInit(T self)
     {
         if (Instance != null) {
             Debug.LogWarning("싱글톤이 이미 존재합니다. 이 오브젝트를 파괴합니다.");
